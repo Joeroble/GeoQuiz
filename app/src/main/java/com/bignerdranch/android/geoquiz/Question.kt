8ae87c93d4c5bcfdc6ce2aea313525292409ad8a
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-01-28 10:06:32 -0600

    Fixed the issue and added comments

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -1,5 +1,5 @@
 package com.bignerdranch.android.geoquiz
 
 import androidx.annotation.StringRes
-
+// creates the dat aclass Question that is used to store the question and answer data.
 data class Question(@StringRes val textResID: Int, val answer: Boolean)
